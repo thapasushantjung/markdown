b8150ff1cc5e24b163bc86f578e91d6cfe312a2a
Sushant Thapa <sushant46bit23@kcc.edu.np>
2025-07-10 11:01:56 +0000

    Added prose to the tailwind config

--- resources/js/pages/index.tsx
+++ resources/js/pages/index.tsx
@@ -1,12 +1,19 @@
+import React, { useState } from 'react';
 const index = () => {
+    const [value, setValue] = useState('');
+    const handleChange = async (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setValue(event.target.value);
+    };
     return (
         <div className="flex h-screen flex-col">
             <div className="flex h-auto">ToolBar</div>
             <div className="flex h-full">
                 <div className="h-full w-1/2">
-                    <textarea className="h-full w-full" placeholder="Start writing here"></textarea>
+                    <textarea value={value} onChange={handleChange} className="h-full w-full" placeholder="Start writing here"></textarea>
                 </div>
-                <div className="h-full w-1/2"></div>
+                <div className="prose h-full w-1/2">
+                    <h1>Hello</h1>
+                </div>
             </div>
         </div>
     );
